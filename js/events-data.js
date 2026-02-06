aa4466858fb4e126692e1a086883164a7a513065
PointlessOtter <39235428+PointlessOtter@users.noreply.github.com>
2026-02-06 18:00:18 +0200

    Hide event: Niršana Maltā

--- js/events-data.js
+++ js/events-data.js
@@ -16,72 +16,68 @@
  */
 
 window.SCUBA_EVENTS = [
-    // 2026
     {
-        title: "Agia Pelagia, Krēta",
-        dateStart: "2026-07-03",
-        dateEnd: "2026-07-10",
-        image: "assets/images/events/crete.png",
-        documentUrl: "assets/documents/Crete_offer_SCUBALIFE_2026.pdf",
-        active: true
-    },
-    // 2025
-    {
-        title: "Marsa Alam, Ēģipte",
-        dateStart: "2025-11-11",
-        dateEnd: "2025-11-19",
-        image: "img/egipte.png",
-        documentUrl: "assets/documents/Marsa_Alam_offer_SCUBALIFE_2025.pdf",
-        active: true
+        "title": "Agia Pelagia, Krēta",
+        "dateStart": "2026-07-03",
+        "dateEnd": "2026-07-10",
+        "image": "assets/images/events/crete.png",
+        "documentUrl": "assets/documents/Crete_offer_SCUBALIFE_2026.pdf",
+        "active": true
     },
     {
-        title: "Niršanas ceļojums uz Kipru",
-        dateStart: "2025-06-26",
-        dateEnd: "2025-07-03",
-        image: "img/pasakums.png",
-        documentUrl: "assets/documents/Cyprus_offer_SCUBALIFE_2025.pdf",
-        active: true
-    },
-    // 2024
-    {
-        title: "Dahaba, Ēģipte",
-        dateStart: "2024-10-29",
-        dateEnd: "2024-11-05",
-        image: "img/pasakums_okt_24.jpeg",
-        documentUrl: null,
-        active: true
+        "title": "Marsa Alam, Ēģipte",
+        "dateStart": "2025-11-11",
+        "dateEnd": "2025-11-19",
+        "image": "img/egipte.png",
+        "documentUrl": "assets/documents/Marsa_Alam_offer_SCUBALIFE_2025.pdf",
+        "active": true
     },
     {
-        title: "Polijas dziļākais ezers Hańcza",
-        dateStart: "2024-09-13",
-        dateEnd: "2024-09-15",
-        image: "img/pasakums_sep_24.jpeg",
-        documentUrl: null,
-        active: true
+        "title": "Niršanas ceļojums uz Kipru",
+        "dateStart": "2025-06-26",
+        "dateEnd": "2025-07-03",
+        "image": "img/pasakums.png",
+        "documentUrl": "assets/documents/Cyprus_offer_SCUBALIFE_2025.pdf",
+        "active": true
     },
     {
-        title: "Atklāj zemūdens noslēpumus, Malta",
-        dateStart: "2024-06-25",
-        dateEnd: "2024-07-02",
-        image: "img/pasakums_jun_24.jpeg",
-        documentUrl: null,
-        active: true
-    },
-    // 2020
-    {
-        title: "Dahaba, Ēģipte",
-        dateStart: "2020-11-06",
-        dateEnd: "2020-11-15",
-        image: "img/jaun_3.jpg",
-        documentUrl: null,
-        active: true
+        "title": "Dahaba, Ēģipte",
+        "dateStart": "2024-10-29",
+        "dateEnd": "2024-11-05",
+        "image": "img/pasakums_okt_24.jpeg",
+        "documentUrl": null,
+        "active": true
     },
     {
-        title: "Niršana Maltā",
-        dateStart: "2020-06-26",
-        dateEnd: "2020-07-04",
-        image: "img/KAL/jun/malta.png",
-        documentUrl: null,
-        active: true
+        "title": "Polijas dziļākais ezers Hańcza",
+        "dateStart": "2024-09-13",
+        "dateEnd": "2024-09-15",
+        "image": "img/pasakums_sep_24.jpeg",
+        "documentUrl": null,
+        "active": true
+    },
+    {
+        "title": "Atklāj zemūdens noslēpumus, Malta",
+        "dateStart": "2024-06-25",
+        "dateEnd": "2024-07-02",
+        "image": "img/pasakums_jun_24.jpeg",
+        "documentUrl": null,
+        "active": true
+    },
+    {
+        "title": "Dahaba, Ēģipte",
+        "dateStart": "2020-11-06",
+        "dateEnd": "2020-11-15",
+        "image": "img/jaun_3.jpg",
+        "documentUrl": null,
+        "active": true
+    },
+    {
+        "title": "Niršana Maltā",
+        "dateStart": "2020-06-26",
+        "dateEnd": "2020-07-04",
+        "image": "img/KAL/jun/malta.png",
+        "documentUrl": null,
+        "active": false
     }
 ];
